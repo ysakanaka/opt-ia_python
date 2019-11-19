5e4991d773239583161bcda1ef1541d6e2ffd5a2
ysakanaka <g1520514@is.ocha.ac.jp>
2019-11-19 18:55:18 +0900

    Implementing logging system.

--- log.py
+++ log.py
@@ -1,27 +1,37 @@
 import logging
 import csv
 import io
+#import optIA
 
 
 class CsvFormatter(logging.Formatter):
     def __init__(self):
         super().__init__()
         self.output = io.StringIO()
-        self.writer = csv.writer(self.output, quoting=csv.QUOTE_ALL)
+        self.writer = csv.writer(self.output)
 
     def format(self, record):
-        #self.writer.writerow([record.levelname, record.msg])
-        self.writer.writerow([record.msg])
+        self.writer.writerow([record.__dict__.get(
+            'generation'), record.__dict__.get(
+            'surplus_at_select')])
+        #self.writer.writerow([record.levelname, record.msg,
+        #                     record.__dict__.get('event', {})])
+        #self.writer.writerow([record.msg])
         data = self.output.getvalue()
         self.output.truncate(0)
         self.output.seek(0)
         return data.strip()
 
-#logging.basicConfig(filename="logtest.csv", level=logging.DEBUG)
+
+#data = {"event": "This is event data", "argtest": 2}
+
+#logging.basicConfig(filename="logtest.csv", filemode='w', level=logging.INFO)
 
 #logger = logging.getLogger(__name__)
 #logging.root.handlers[0].setFormatter(CsvFormatter())
 
 #logger.debug('This message should appear on the console')
-#logger.info('So should "this", and it\'s using quoting...')
+#logger.info('This is test message the argument is %s', 2)
 #logger.warning('And this, too')
+
+#logger.info("dict info test", extra=dict(data))
